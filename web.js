2118d3b3f9ccc8bb9209a623bedac7168ae7a1a9
animekraxe <yukibhuynh@gmail.com>
2013-07-10 18:51:07 +0000

    From index.html

--- web.js
+++ web.js
@@ -1,12 +1,19 @@
+var fs = require('fs');
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var filestring = fs.readFileSync('index.html').toString();
+
+console.log(filestring);
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(filestring);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
