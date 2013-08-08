c3231918f5701fc163c66029ad8119699dab8ee8
animekraxe <yukibhuynh@gmail.com>
2013-08-08 05:43:23 +0000

    Fixed index in develop

--- web.js
+++ web.js
@@ -6,13 +6,11 @@ var app = express.createServer(express.logger());
 
 var filestring = fs.readFileSync('index.html').toString();
 
-console.log(filestring);
-
 app.get('/', function(request, response) {
   response.send(filestring);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
